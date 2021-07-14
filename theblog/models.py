1039c8b8c6317510a84e7936d4dcef413e926771
royalturd <samridhh.rudra@hotmail.com>
2021-07-15 02:43:36 +0530

    Styling: Style the Blog With Bootstrap
    Signed-off-by: royalturd <samridhh.rudra@hotmail.com>

--- theblog/models.py
+++ theblog/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=225)
+    title_tag = models.CharField(max_length=225, default="My personal blog")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField(null=True)
 
