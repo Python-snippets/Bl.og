5fde52bd360da7f0716df5a49ecb86ea95c43b96
royalturd <samridhh.rudra@hotmail.com>
2021-10-14 04:14:28 +0530

    Bl.og: added and styled 'Edit profile' and 'Change user password page' �
    Signed-off-by: royalturd <samridhh.rudra@hotmail.com>

--- members/forms.py
+++ members/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import PasswordChangeForm, UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
 from django import forms
 
@@ -17,3 +17,23 @@ class SignUpForm(UserCreationForm):
         self.fields['username'].widget.attrs['class'] = 'form-control'
         self.fields['password1'].widget.attrs['class'] = 'form-control'
         self.fields['password2'].widget.attrs['class'] = 'form-control'
+
+class EditProfileForm(UserChangeForm):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(max_length=100,  widget=forms.TextInput(attrs={'class': 'form-control'}))
+    username = forms.CharField(max_length=100,  widget=forms.TextInput(attrs={'class': 'form-control'}))
+    date_joined = forms.CharField(max_length=100,  widget=forms.TextInput(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model  = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'date_joined', 'password')
+
+class PasswordChangedForm(PasswordChangeForm):
+    old_password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'type':'password'}))
+    new_password1 = forms.CharField(max_length=100, widget=forms.PasswordInput(attrs={'class': 'form-control', 'type':'password'}))
+    new_password2 = forms.CharField(max_length=100,  widget=forms.PasswordInput(attrs={'class': 'form-control', 'type':'password'}))
+
+    class Meta:
+        model = User
+        fields = ('old_password', 'new_password1', 'new_password2' )
