466c1f68032b429e961b2587df0603304a27d401
royalturd <samridhh.rudra@hotmail.com>
2026-03-15 23:59:10 +0530

    chore: update requirements.txt and fix settings for Django 5
    Signed-off-by: royalturd <samridhh.rudra@hotmail.com>

--- ablog/settings.py
+++ ablog/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'ablog.urls'
@@ -120,8 +121,6 @@ LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
-
-USE_L10N = True
 
 USE_TZ = True
 
